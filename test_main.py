bc5ba4b49fb1c059926bb544b745c0183dd0ab83
Witold Przygoda <wprzygoda@pwsz-ns.edu.pl>
2024-10-13 23:20:16 +0200

    test code

---
+++ test_main.py
@@ -0,0 +1,33 @@
+from main import przetworz_wejscie
+
+def test_przetworz_wejscie_standard_input():
+    wejscie = "   ,,,WITAJ, ŚWIECIE!!! To jest przykładowy TEKST, który będzie OBRABIANy,,,   "
+    expected_lista = ['witaj', 'świecie!!!', 'to', 'jest', 'przykładowy', 'tekst', 'który', 'będzie', 'obrabiany']
+    expected_liczba = 9
+    wynik_lista, wynik_liczba = przetworz_wejscie(wejscie)
+    assert wynik_lista == expected_lista
+    assert wynik_liczba == expected_liczba
+
+def test_przetworz_wejscie_z_pustymi_elementami():
+    wejscie = ",,, , alfa,, beta,,gamma,"
+    expected_lista = ['alfa', 'beta', 'gamma']
+    expected_liczba = 3
+    wynik_lista, wynik_liczba = przetworz_wejscie(wejscie)
+    assert wynik_lista == expected_lista
+    assert wynik_liczba == expected_liczba
+
+def test_przetworz_wejscie_z_malymi_literami():
+    wejscie = "   Python, PYTHON , python"
+    expected_lista = ['python', 'python', 'python']
+    expected_liczba = 3
+    wynik_lista, wynik_liczba = przetworz_wejscie(wejscie)
+    assert wynik_lista == expected_lista
+    assert wynik_liczba == expected_liczba
+
+def test_przetworz_wejscie_puste():
+    wejscie = "   ,, ,,,   "
+    expected_lista = []
+    expected_liczba = 0
+    wynik_lista, wynik_liczba = przetworz_wejscie(wejscie)
+    assert wynik_lista == expected_lista
+    assert wynik_liczba == expected_liczba
